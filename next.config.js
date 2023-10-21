07299356542e6a34162a2d040204b5f55f67254d
Connor <hconnora@outlook.com>
2023-10-21 21:43:11 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
+
 const nextConfig = {
   typescript: {
     // !! WARN !!
@@ -25,4 +23,4 @@ const nextConfig = {
     }
   }
   
-  module.exports = nextConfig;
+  module.exports = nextConfig
